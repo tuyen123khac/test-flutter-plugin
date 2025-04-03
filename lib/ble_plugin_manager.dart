b16787f404bfd82a7f562277d8472165b39d37ed
Tuyen Vo <khactuyen.vo@citynow.vn>
2025-04-03 22:25:18 +0700

    delete

--- lib/ble_plugin_manager.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ble_plugin_test/application/permission_utils.dart';
-import 'package:ble_plugin_test/plugin/device_core/device_core_plugin.dart';
-import 'package:ble_plugin_test/plugin/device_core/device_event_task.dart';
-import 'package:ble_plugin_test/plugin/device_core/enum/event_enum.dart';
-import 'package:ble_plugin_test/plugin/device_core/models/found_device.dart';
-
-class BlePluginManager {
-  
-}
